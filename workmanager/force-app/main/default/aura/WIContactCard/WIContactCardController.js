59ceaaa0bfd1ef02f8147f03e577c6b6305921db
Christian Estol <cestol@altimetrik.com>
2019-06-21 09:54:31 -0300

    part 3

---
+++ workmanager/force-app/main/default/aura/WIContactCard/WIContactCardController.js
@@ -0,0 +1,32 @@
+({
+    workIcon : function(component, event, helper) {
+        var cardType = component.get("v.workItem.Type__c")
+        
+        if(cardType == "Idea"){
+            component.set("v.iconType","action:description");
+        }else if(cardType == "Action"){
+            component.set("v.iconType","action:fallback");
+        }else if(cardType == "Fix"){
+            component.set("v.iconType","action:canvas");
+        }else{
+            return null;
+        }
+        
+    },
+    
+    editRecord : function (component, event, helper){
+        var editRecordEvent = $A.get("e.force:editRecord");
+        editRecordEvent.setParams({
+            "recordId": component.get("v.workItem.Id")
+        });
+        editRecordEvent.fire();
+    },
+    
+    openRecord : function (component, event, helper) {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId": component.get("v.workItem.Id")
+        });
+        navEvt.fire();
+    },
+})
